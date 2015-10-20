8b93a5f335d192d225ca351281f64c1379242d46
tomseldon <tom@tomseldon.co.uk>
2015-10-21 00:59:35 +0100

    Output: Use uiSref instead of controller action

--- app/states/output/output.controller.js
+++ app/states/output/output.controller.js
@@ -7,26 +7,12 @@
     /**
      * Controller constructor
      *
-     * @param {ui.router.$state} $state
      * @param {string} output - The generated markdown
      */
     function OutputController($state, output) {
-        /**
-         * @type {ui.router.$state}
-         * @private
-         */
-        this._$state = $state;
-
         /**
          * @type {string}
          */
         this.markdown = output;
     }
-
-    /**
-     * Go back to the start so a new conversion can take place
-     */
-    OutputController.prototype.goBackToStart = function() {
-        this._$state.go('home');
-    };
 })(window.angular);
