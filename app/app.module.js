4f41df4ec5011323ebf1548212a443a266d98e4d
tomseldon <tom@tomseldon.co.uk>
2015-10-25 00:12:56 +0100

    [refactor] app module - CS changes

--- app/app.module.js
+++ app/app.module.js
@@ -1,11 +1,12 @@
-(function(Office, angular){
+(function() {
   'use strict';
 
-  var appName = 'word-to-markdown';
   var officeInitialised = false;
-  var initialisationMaxTime = 3000; // Max time in ms before we report error that we haven't started
 
-  angular.module(appName, [
+  // Max time in ms before we report error that we haven't started
+  var initialisationMaxTime = 3000;
+
+  angular.module('word-to-markdown', [
     // Vendor
     'ngAnimate',
     'ngSanitize',
@@ -19,18 +20,22 @@
     'word-to-markdown.states.home',
     'word-to-markdown.states.output'
   ])
-      .run(goToInitialState)
-      .config(debugLogging);
+    .run(goToInitialState)
+    .config(debugLogging);
 
   /**
-   * @param {ui.router.$state} $state
+   * @param {ui.router.$state} $state - Service for transitioning between UI states
+   *
+   * @returns {undefined}
    */
   function goToInitialState($state) {
     $state.go('home');
   }
 
   /**
-   * @param {angular.$logProvider} $logProvider
+   * @param {angular.$logProvider} $logProvider - Provider for configuring log service
+   *
+   * @returns {undefined}
    */
   function debugLogging($logProvider) {
     // set debug logging to on
@@ -41,13 +46,15 @@
 
   /**
    * Start the Angular app
+   *
+   * @returns {undefined}
    */
   function bootstrap() {
-    angular.bootstrap(document.getElementById('container'), [appName]);
+    angular.bootstrap(document.getElementById('container'), ['word-to-markdown']);
   }
 
   // when Office has initalized, manually bootstrap the app
-  Office.initialize = function(){
+  Office.initialize = function() {
     console.log('>>> Office.initialize()');
     officeInitialised = true;
     bootstrap();
@@ -57,11 +64,12 @@
   // To work around this, we can expose a manual way of starting the app if we haven't been initialised after a set time
   setTimeout(function() {
     if (!officeInitialised) {
-      console.error('App wasn\'t initialised in time. Use `window.wordToMarkDown.start` to manually bootstrap the application');
+      console.error(
+        'App wasn\'t initialised in time. Use `window.wordToMarkDown.start` to manually bootstrap the application'
+      );
     }
 
     window.wordToMarkDown = window.wordToMarkDown || {};
     window.wordToMarkDown.start = bootstrap;
   }, initialisationMaxTime);
-
-})(window.Office, window.angular);
+})();
