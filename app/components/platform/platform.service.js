e5c9841e1738abe0821c001787ca8c611349a3a5
tomseldon <tom@tomseldon.co.uk>
2015-10-24 23:19:58 +0100

    [refactor] platform service - CS changes

--- app/components/platform/platform.service.js
+++ app/components/platform/platform.service.js
@@ -1,29 +1,29 @@
-(function(angular, Office) {
-    'use strict';
+(function() {
+  'use strict';
 
-    angular.module('word-to-markdown.platform', [])
-        .service('platform', PlatformService);
+  angular.module('word-to-markdown.platform', [])
+    .service('platform', PlatformService);
 
-    /**
-     * @class PlatformService
-     * @constructor
-     */
-    function PlatformService() {
-    }
+  /**
+   * @class PlatformService
+   * @constructor
+   */
+  function PlatformService() {
+  }
 
-    /**
-     * @returns {boolean}
-     */
-    PlatformService.prototype.isRunningInBrowser = function() {
-        return !this.isRunningInOffice();
-    };
+  /**
+   * @returns {boolean} Returns true if not running in a Microsoft Office environment
+   */
+  PlatformService.prototype.isRunningInBrowser = function() {
+    return !this.isRunningInOffice();
+  };
 
-    /**
-     * @returns {boolean}
-     */
-    PlatformService.prototype.isRunningInOffice = function() {
-        var hasContext = typeof Office.context !== 'undefined';
+  /**
+   * @returns {boolean} Returns true if we're running in a Microsoft Office environment
+   */
+  PlatformService.prototype.isRunningInOffice = function() {
+    var hasContext = typeof Office.context !== 'undefined';
 
-        return hasContext && typeof Office.context.document !== 'undefined';
-    };
-})(window.angular, window.Office);
+    return hasContext && typeof Office.context.document !== 'undefined';
+  };
+})();
