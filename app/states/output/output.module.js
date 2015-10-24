663fd6ec791c48a3319e73e16cc8ea8ee336bfa0
tomseldon <tom@tomseldon.co.uk>
2015-10-24 23:17:09 +0100

    [refactor] output module - CS changes

--- app/states/output/output.module.js
+++ app/states/output/output.module.js
@@ -1,30 +1,31 @@
-(function(angular) {
-    'use strict';
+(function() {
+  'use strict';
 
-    angular.module('word-to-markdown.states.output', [
-        'ui.router',
-        'word-to-markdown.states.output.controller'
-    ])
-        .config(routeConfigurator);
+  angular.module('word-to-markdown.states.output', [
+    'ui.router',
+    'word-to-markdown.states.output.controller'
+  ])
+    .config(routeConfigurator);
 
-    /**
-     * @param {ui.router.$stateProvider} $stateProvider
-     */
-    function routeConfigurator($stateProvider) {
-        $stateProvider.state('output', {
-            templateUrl: 'app/states/output/output.html',
-            controller: 'OutputController as vm',
-            resolve: {
-                output: getMarkdownForSelectedText
-            }
-        });
-    }
+  /**
+   * @param {ui.router.$stateProvider} $stateProvider - Provider for registering UI states
+   * @returns {undefined}
+   */
+  function routeConfigurator($stateProvider) {
+    $stateProvider.state('output', {
+      templateUrl: 'app/states/output/output.html',
+      controller: 'OutputController as vm',
+      resolve: {
+        output: getMarkdownForSelectedText
+      }
+    });
+  }
 
-    /**
-     * @param {MarkdownConverterService} markdownConverter
-     * @returns {Promise.<string>}
-     */
-    function getMarkdownForSelectedText(markdownConverter) {
-        return markdownConverter.convertSelectedText();
-    }
-})(window.angular);
+  /**
+   * @param {MarkdownConverterService} markdownConverter - Service for converting data to markdown
+   * @returns {Promise.<string>} The markdown representation of the selected text
+   */
+  function getMarkdownForSelectedText(markdownConverter) {
+    return markdownConverter.convertSelectedText();
+  }
+})();
