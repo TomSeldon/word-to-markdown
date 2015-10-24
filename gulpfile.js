8ece43ccd1b41ed61d651b57b03c200f65f6d464
tomseldon <tom@tomseldon.co.uk>
2015-10-24 23:05:13 +0100

    [refactor] Fix CS issues in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,58 +1,59 @@
-'use script';
-
 var gulp = require('gulp');
 var webserver = require('gulp-webserver');
 var eslint = require('gulp-eslint');
 var fs = require('fs');
+var path = require('path');
 var minimist = require('minimist');
 var xmllint = require('xmllint');
 var chalk = require('chalk');
 
 gulp.task('karma:ci', function(done) {
   var Server = require('karma').Server;
+  var configFile = path.join(__dirname, 'karma.conf.js');
 
   new Server({
-    configFile: __dirname + '/karma.conf.js',
+    configFile: configFile,
     singleRun: true
   }, done)
-      .start();
+    .start();
 });
 
 gulp.task('karma:tdd', function(done) {
   var Server = require('karma').Server;
+  var configFile = path.join(__dirname, 'karma.conf.js');
 
   new Server({
-    configFile: __dirname + '/karma.conf.js',
+    configFile: configFile,
     singleRun: false
   }, done)
-      .start();
+    .start();
 });
 
 gulp.task('coveralls', function() {
   var coveralls = require('gulp-coveralls');
 
   gulp.src('coverage/**/lcov.info')
-      .pipe(coveralls());
+    .pipe(coveralls());
 });
 
 gulp.task('serve-static', function() {
   gulp.src('.')
-      .pipe(webserver({
-        https: true,
-        port: '8443',
-        host: 'localhost',
-        directoryListing: true,
-        fallback: 'index.html'
-      }));
+    .pipe(webserver({
+      https: true,
+      port: '8443',
+      host: 'localhost',
+      directoryListing: true,
+      fallback: 'index.html'
+    }));
 });
 
 gulp.task('eslint:src', function() {
   return gulp.src(['app/**/*.js', '!app/**/*.test.js'])
-      .pipe(eslint({
-        configFile: '.eslintrc-src'
-      }))
-      .pipe(eslint.format())
-      .pipe(eslint.failAfterError());
+    .pipe(eslint({
+      configFile: '.eslintrc-src'
+    }))
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
 gulp.task('eslint:unit-tests', function() {
@@ -82,7 +83,7 @@ gulp.task('eslint:config', function() {
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('validate-xml', function () {
+gulp.task('validate-xml', function() {
   var options = minimist(process.argv.slice(2));
   var xsd = fs.readFileSync('./manifest.xsd');
   var xmlFilePath = options.xmlfile || './manifest.xml';
@@ -90,7 +91,7 @@ gulp.task('validate-xml', function () {
   var xml = fs.readFileSync(xmlFilePath);
 
   if (!resultsAsJson) {
-    console.log('\nValidating ' + chalk.blue(xmlFilePath.substring(xmlFilePath.lastIndexOf('/')+1)) + ':');
+    console.log('\nValidating ' + chalk.blue(xmlFilePath.substring(xmlFilePath.lastIndexOf('/') + 1)) + ':');
   }
   var result = xmllint.validateXML({
     xml: xml,
@@ -101,24 +102,25 @@ gulp.task('validate-xml', function () {
 
   if (resultsAsJson) {
     console.log(JSON.stringify(result));
-  }
-  else {
-    if (result.errors === null) {
-      console.log(chalk.green('Valid'));
-    }
-    else {
-      console.log(chalk.red('Invalid'));
-      result.errors.forEach(function(e) {
-        console.log(chalk.red(e));
-      });
-    }
+  } else if (result.errors === null) {
+    console.log(chalk.green('Valid'));
+  } else {
+    console.log(chalk.red('Invalid'));
+    result.errors.forEach(function(e) {
+      console.log(chalk.red(e));
+    });
   }
 });
 
+/**
+ * @param {*} xml - XML content of manifest
+ * @param {*} result - Result object
+ * @returns {undefined}
+ */
 function validateHighResolutionIconUrl(xml, result) {
   if (xml && result) {
     if (xml.indexOf('<HighResolutionIconUrl ') > -1 &&
-        xml.indexOf('<HighResolutionIconUrl DefaultValue="https://') < 0) {
+      xml.indexOf('<HighResolutionIconUrl DefaultValue="https://') < 0) {
       if (result.errors === null) {
         result.errors = [];
       }
