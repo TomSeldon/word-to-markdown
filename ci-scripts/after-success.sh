8cce38d09c45093f314f9e9ceae2cfd9366e205f
tomseldon <tom@tomseldon.co.uk>
2015-10-25 01:00:28 +0100

    [config] Fix after success CI script

--- ci-scripts/after-success.sh
+++ ci-scripts/after-success.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ev
 
-# Update coveralls if this was a unit test build
-if [ "${BUILD}" = "static-no-linting" ]; then
+# Update coveralls if this was a static test build
+if [ "${BUILD}" = "static" ]; then
 	node_modules/.bin/gulp coveralls
 fi
