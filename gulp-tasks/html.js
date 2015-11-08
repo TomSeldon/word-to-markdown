52ad528734200be26f19e8c62bbaf7295ef13379
tomseldon <tom@tomseldon.co.uk>
2015-11-08 14:35:04 +0000

    [config] minify index.html on build

--- gulp-tasks/html.js
+++ gulp-tasks/html.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
+var htmlmin = require('gulp-htmlmin');
 
 gulp.task('html', function() {
   return gulp.src('index.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
     .pipe(gulp.dest('dist'));
 });
