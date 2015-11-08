57c194568d30a896bf24fe89ade92f45a648fb30
tomseldon <tom@tomseldon.co.uk>
2015-11-08 14:26:10 +0000

    [config] make gulp serve-static work with dist folder

--- gulp-tasks/serve-static.js
+++ gulp-tasks/serve-static.js
@@ -2,13 +2,19 @@ var gulp = require('gulp');
 
 gulp.task('serve-static', function() {
   var webserver = require('gulp-webserver');
+  var rewrite = require('connect-url-rewrite');
 
   gulp.src('.')
     .pipe(webserver({
       https: true,
       port: '8443',
       host: '0.0.0.0',
-      directoryListing: true,
-      fallback: 'index.html'
+      middleware: rewrite([
+        '^/index.html /dist/index.html',
+        '^/vendor/(.+) /bower_components/$1',
+        '^/app/(.+) /dist/app/$1',
+        '^/css/(.+) /dist/css/$1',
+        '^/images/(.+) /dist/images/$1'
+      ])
     }));
 });
